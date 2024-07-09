4ee6c63b3a4f5259aa55861317fa043a3d180765
Oussama Elhouari <oussamaelh@gmail.com>
2024-07-09 15:02:15 +0100

    Handling Exceptions in .Net

---
+++ Program.cs
@@ -0,0 +1,192 @@
+﻿// Throw exceptions examples
+
+try
+{
+    OperatingProcedure1();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Exiting application.");
+}
+
+static void OperatingProcedure1()
+{
+    string[][] userEnteredValues = new string[][]
+    {
+        new string[] { "1", "two", "3"},
+        new string[] { "0", "1", "2"}
+    };
+
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            BusinessProcess1(userEntries);
+        }
+        catch (Exception ex)
+        {
+            if (ex.StackTrace.Contains("BusinessProcess1"))
+            {
+                if (ex is FormatException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Corrective action taken in OperatingProcedure1");
+                }
+                else if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+                    // re-throw the original exception
+                    throw;
+                }
+                else
+                {
+                    // create a new exception object that wraps the original exception
+                    throw new ApplicationException("An error occurred - ", ex);
+                }
+            }
+        }
+
+    }
+}
+
+static void BusinessProcess1(string[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        try
+        {
+            valueEntered = int.Parse(userValue);
+
+            checked
+            {
+                int calculatedValue = 4 / valueEntered;
+            }
+        }
+        catch (FormatException)
+        {
+            FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+            throw invalidFormatException;
+        }
+        catch (DivideByZeroException)
+        {
+            DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+            throw unexpectedDivideByZeroException;
+
+        }
+    }
+}
+
+
+// try
+// {
+//     int num1 = int.MaxValue;
+//     int num2 = int.MaxValue;
+//     int result = num1 + num2;
+//     Console.WriteLine("Result: " + result);
+
+//     string str = null;
+//     int length = str.Length;
+//     Console.WriteLine("String Length: " + length);
+
+//     int[] numbers = new int[5];
+//     numbers[5] = 10;
+//     Console.WriteLine("Number at index 5: " + numbers[5]);
+
+//     int num3 = 10;
+//     int num4 = 0;
+//     int result2 = num3 / num4;
+//     Console.WriteLine("Result: " + result2);
+// }
+// catch (OverflowException ex)
+// {
+//     Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
+// }
+// catch (NullReferenceException ex)
+// {
+//     Console.WriteLine("Error: The reference is null." + ex.Message);
+// }
+// catch (IndexOutOfRangeException ex)
+// {
+//     Console.WriteLine("Error: Index out of range." + ex.Message);
+// }
+// catch (DivideByZeroException ex)
+// {
+//     Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+// }
+
+// Console.WriteLine("Exiting program.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try
+// {
+//     Process1();
+// }
+// catch
+// {
+//     Console.WriteLine("An exception has occurred");
+// }
+
+// Console.WriteLine("Exit program");
+
+// static void Process1()
+// {
+//     try
+//     {
+//         WriteMessage();
+//     }
+//     catch (DivideByZeroException ex)
+//     {
+//         Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+//     }
+// }
+
+// static void WriteMessage()
+// {
+//     double float1 = 3000.0;
+//     double float2 = 0.0;
+//     int number1 = 3000;
+//     int number2 = 0;
+//     byte smallNumber;
+
+//     try
+//     {
+//         Console.WriteLine(float1 / float2);
+//         Console.WriteLine(number1 / number2);
+//     }
+//     catch (DivideByZeroException ex)
+//     {
+//         Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+//     }
+//     checked
+//     {
+//         smallNumber = (byte)number1;
+//     }
+// }
+
+// // static void WriteMessage()
+// // {
+// //     double float1 = 3000.0;
+// //     double float2 = 0.0;
+// //     int number1 = 3000;
+// //     int number2 = 0;
+
+// //     Console.WriteLine(float1 / float2);
+// //     Console.WriteLine(number1 / number2);
+// // }
+
